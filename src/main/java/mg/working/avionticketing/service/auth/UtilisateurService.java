6161dba84b34b0bc95aa1834f026c19d3b509ffd
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-07-04 10:07:19 +0300

    Fonction Login Service , Cryptage Mot de passe

---
+++ src/main/java/mg/working/avionticketing/service/auth/UtilisateurService.java
@@ -0,0 +1,33 @@
+package mg.working.avionticketing.service.auth;
+
+import mg.working.avionticketing.entity.user.Utilisateur;
+import mg.working.avionticketing.repository.auth.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UtilisateurService {
+    @Autowired
+    UtilisateurRepository utilisateurRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Utilisateur getUtilisateurById(int id) {
+        return utilisateurRepository.findById(id).orElse(null);
+    }
+
+    public void insertUtilisateur(Utilisateur utilisateur) {
+        utilisateurRepository.save(utilisateur);
+    }
+
+    public Utilisateur getUtilisateurByEmailAndMotDePasse(String email , String motDePasse) {
+        Utilisateur utilisateur = utilisateurRepository.findByEmail(email).orElse(null);
+        if (utilisateur != null && passwordEncoder.matches(motDePasse , utilisateur.getMotDePasse())) {
+            return utilisateur;
+        }
+        return null;
+    }
+
+}
